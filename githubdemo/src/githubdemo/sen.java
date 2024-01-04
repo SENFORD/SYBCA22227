1883cc7b22f59043df7fa84501eceb06e2ba1083
Senford Fernandes <senfordfernandes201@gmail.com>
2024-01-04 10:45:34 +0530

    commit second

--- githubdemo/src/githubdemo/sen.java
+++ githubdemo/src/githubdemo/sen.java
@@ -5,7 +5,10 @@ public class sen {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-
+        System.out.println("GitHub Hello World");
+	}
+	void display() {
+		System.out.println("ARWIN do not use my github account");
 	}
 
 }
