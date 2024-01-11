285227629ac098a48f20c7b76537f0fbb55a04fe
Senford Fernandes <senfordfernandes201@gmail.com>
2024-01-11 10:59:57 +0530

    sub

---
+++ githubdemo/src/githubdemo/sub.java
@@ -0,0 +1,19 @@
+package githubdemo;
+
+import java.util.Scanner;
+
+public class sub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,sub;
+		System.out.println("Enter two numbers");
+		Scanner scan=new Scanner(System.in);
+		a=scan.nextInt();
+		b=scan.nextInt();
+		sub=a-b;
+		System.out.println("The sum is : "+sub);
+
+	}
+
+}
