f36481cbd00a409ea488ffbbfb3a9306b033dac0
Senford Fernandes <senfordfernandes201@gmail.com>
2024-01-11 10:40:00 +0530

    add commit

---
+++ githubdemo/src/githubdemo/add.java
@@ -0,0 +1,19 @@
+package githubdemo;
+
+import java.util.Scanner;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,sum;
+		System.out.println("Enter two numbers");
+		Scanner scan=new Scanner(System.in);
+		a=scan.nextInt();
+		b=scan.nextInt();
+		sum=a+b;
+		System.out.println("The sum is : "+sum);
+
+	}
+
+}
