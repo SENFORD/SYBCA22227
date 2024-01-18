4a4b3d7ebeb36d9b9903448d9d335dce44e80e70
Senford Fernandes <senfordfernandes201@gmail.com>
2024-01-18 09:47:24 +0530

    hiiiii

---
+++ githubproject/Assignment227/src/assignment/add.java
@@ -0,0 +1,19 @@
+package assignment;
+
+import java.util.Scanner;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,y,sum;
+		System.out.println("Enter two numbers");
+		Scanner scan=new Scanner(System.in);
+		a=scan.nextInt();
+		y=scan.nextInt();
+		sum=a+y;
+		System.out.println("The sum is : "+sum);
+
+	}
+
+}
